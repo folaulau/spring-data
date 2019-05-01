e9e80c18bfdf4ac20d9e9b8fbe3a49f6c2347ba7
Folau <folaukaveinga@gmail.com>
2019-04-30 22:41:52 -0700

    add all dto

---
+++ spring-data-dto/src/main/java/com/lovemesomecoding/loader/UserLoader.java
@@ -0,0 +1,60 @@
+package com.lovemesomecoding.loader;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.lovemesomecoding.address.Address;
+import com.lovemesomecoding.role.Role;
+import com.lovemesomecoding.role.RoleService;
+import com.lovemesomecoding.user.User;
+import com.lovemesomecoding.user.UserService;
+import com.lovemesomecoding.utils.ObjectUtils;
+import com.lovemesomecoding.utils.RandomGeneratorUtils;
+
+@Component
+public class UserLoader {
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private UserService userService;
+	
+	@PostConstruct
+	public void loadUsers() {
+		log.info("loadUsers...");
+		
+		User user = new User();
+		user.setUid(RandomGeneratorUtils.getUuid());
+		user.setAge(21);
+		user.setName("Folau");
+		user.setEmail("folaukaveinga@gmail.com");
+		
+		Address address = new Address();
+		address.setCity("El Segundo");
+		address.setState("CA");
+		address.setStreet("Rosescrans");
+		address.setZip("90305");
+		
+		user.setAddress(address);
+		
+		log.info("pre persist: {}",ObjectUtils.toJson(user));
+		
+		user = userService.create(user);
+		
+		log.info("post persist: {}",ObjectUtils.toJson(user));
+		
+		user = userService.getById(user.getId());
+		
+		log.info("retrieve: {}",ObjectUtils.toJson(user));
+		
+		log.info("User has been loaded!");
+	}
+}
--- spring-data-many-to-many/src/main/java/com/lovemesomecoding/coverage/CoverageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lovemesomecoding.coverage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoverageRepository extends JpaRepository<Coverage, Long>{
-
-}
